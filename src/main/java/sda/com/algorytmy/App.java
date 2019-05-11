a4164290eaf77e134b5619a5e634b1d595d0cfce
Szymon Luniewski <sizmon15@gmail.com>
2019-05-11 15:48:01 +0100

    ciag2

--- src/main/java/sda/com/algorytmy/App.java
+++ src/main/java/sda/com/algorytmy/App.java
@@ -2,12 +2,42 @@ package sda.com.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+
+    //        public static int silnia ( int n){
+//            return silnia(n);
+//
+//        }
+    public static void main(String[] args) {
+//            int silnia = 5;
+//            int wynik = 1;
+//            for (int i = 1; i <= silnia; i++) {
+//                wynik = wynik * i;
+//            }
+//            System.out.println(wynik);
+
+//        }
+//
+
+    }
+
+    public static int ciag2(int a) {
+        if (a == 1) {
+            return -1;
+        }
+        return -ciag2(a - 1) * a - 3;
     }
 }
+
+
+
+
+
+
+
+
+
+
