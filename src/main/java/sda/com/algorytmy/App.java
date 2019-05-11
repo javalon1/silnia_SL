b4ded9d328cade3d76d0435997710242721dcf69
Szymon Luniewski <sizmon15@gmail.com>
2019-05-11 13:31:20 +0100

    repo init

---
+++ src/main/java/sda/com/algorytmy/App.java
@@ -0,0 +1,13 @@
+package sda.com.algorytmy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
